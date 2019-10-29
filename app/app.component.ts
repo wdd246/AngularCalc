7d7d8f76e1da0cce144740106034e111aacdc5f0
Adam Małota <adammalota@mansio.pl>
2019-10-29 09:25:39 +0100

    Fix

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Binary Calculator';
-  box:any;
   value='';
   values =[];
+  dec:any;
   x:any;
 
-  onKey(value: string) {
-    this.value = value;
+  onChange(event) {
+    this.value = event.target.value;
   }
 
   addOne(){
@@ -24,40 +24,45 @@ export class AppComponent {
     this.value +='0' ;
   }
 
+  clear(){
+    this.dec = '';
+    this.value = '';
+    this.values = [];
+    this.x = '';
+  }
   submit(){
     if(this.values.length>=2){
-    if(this.values[this.values.length]!='-' && this.values[this.values.length]!='+') 
-      this.values[this.values.length]=this.value
-    for(let i=0; i<this.values.length; i++){
-      if(this.values[i]!='+' && this.values[i]!='-'){
-        this.values[i]=parseInt(this.values[i],2)
+      if(this.values[this.values.length]!='-' && this.values[this.values.length]!='+') 
+        this.values[this.values.length]=this.value
+      for(let i=0; i<this.values.length; i++){
+        if(this.values[i]!='+' && this.values[i]!='-'){
+          this.values[i]=parseInt(this.values[i],2)
+        }
       }
+      for(let i=0; i<this.values.length; i++){
+        if(this.values[i-1]=='+'){
+          this.x=this.values[i-2]+this.values[i]
+          //console.log(this.x)
+        }
+        if(this.values[i-1]=='-'){
+          this.x=this.values[i-2]-this.values[i]
+          //console.log(this.x)
+        }
+      }
+      for(let i=0; i<this.values.length; i++){
+        if(this.values[i]!='+' && this.values[i]!='-'){
+          this.values[i]=parseInt(this.values[i],10).toString(2)
+        }
+      }
+      this.dec=this.x;
+      this.value=parseInt(this.x,10).toString(2)
     }
-    for(let i=0; i<this.values.length; i++){
-      if(this.values[i-1]=='+'){
-        this.x=this.values[i-2]+this.values[i]
-        //console.log(this.x)
-      }
-      if(this.values[i-1]=='-'){
-        this.x=this.values[i-2]-this.values[i]
-        //console.log(this.x)
-      }
-    }
-    for(let i=0; i<this.values.length; i++){
-      if(this.values[i]!='+' && this.values[i]!='-'){
-        this.values[i]=parseInt(this.values[i],10).toString(2)
-      }
-    }
-    this.value=parseInt(this.x,10).toString(2)
-  }
   }
 
   plus(){
     if(this.values[this.values.length-1]!='-' && this.values[this.values.length-1]!='+') {
       this.value +='+';
-      this.values[this.values.length]=this.value.substring(0,this.value.length-1);
-      this.values[this.values.length]=this.value[this.value.length-1];
-      this.value='';
+      this.getBinAndClear()
     }
     else{
       this.value
@@ -67,12 +72,16 @@ export class AppComponent {
   minus(){
     if(this.values[this.values.length-1]!='-' && this.values[this.values.length-1]!='+') {
       this.value +='-';
-      this.values[this.values.length]=this.value.substring(0,this.value.length-1);
-      this.values[this.values.length]=this.value[this.value.length-1];
-      this.value='';
+      this.getBinAndClear()
     }
     else{
       this.value;
     }
+  }
+
+  getBinAndClear(){
+    this.values[this.values.length]=this.value.substring(0,this.value.length-1);
+    this.values[this.values.length]=this.value[this.value.length-1];
+    this.value='';
   }
 }
